2261d6e695d9bfc37e228351b60c950bdc196c28
Nickobellic <avrahulkanna17@gmail.com>
2024-07-14 21:39:40 +0530

    add subtitles.py to fetch subtitles from videos along with quiz generation.

---
+++ backend/subtitles.py
@@ -0,0 +1,24 @@
+from youtube_transcript_api import YouTubeTranscriptApi
+from test import generate_quiz
+
+result = YouTubeTranscriptApi.get_transcript("iuK05gGBzJc")
+
+#print(result)
+
+# Get subtitles upto Chemistry basics
+from_seconds = 1334
+to_seconds = 1952
+subtitles = ""
+
+for sub in result:
+    subtitleDuration = sub['start']
+    if(subtitleDuration>to_seconds):
+        break
+    else:
+        if(subtitleDuration>=from_seconds and subtitleDuration<=to_seconds):
+            subtitles += sub['text'] + " "
+
+print(subtitles)
+
+print("Generated Quizzes")
+print(generate_quiz(subtitles))
--- backend/test.py
+++ backend/test.py
@@ -1,12 +1,15 @@
 import ollama
-ollama.pull('llama3')
-response = ollama.chat(model='llama3', messages=[
-  {
-    'role': 'user',
-    'content': 'Why is the sky blue?',
 
-  },
-], stream=True)
 
-for chunk in response:
-  print(chunk['message']['content'], end='', flush=True)
+def generate_quiz(query):
+  ollama.pull('llama3')
+  response = ollama.chat(model='llama3', messages=[
+    {
+      'role': 'user',
+      'content': f'Generate 5 quiz questions related to this. {query}',
+
+    },
+  ], stream=True)
+
+  for chunk in response:
+    print(chunk['message']['content'], end='', flush=True)
